63b5a87b13d92cf5fcce895cfc3b79165a463fd4
Lhaniii <ilhan95210@icloud.com>
2024-07-18 22:55:59 +0200

    complete

---
+++ ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+export default function appendToEachArrayValue(array, appendString) {
+    const NewAr = [];
+
+    for (const idx of array) {
+        NesAr.push(appendString + idx);
+    }
+  
+    return NewAr;
+}
