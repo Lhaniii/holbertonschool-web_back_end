c7862af6771142b3cbc25f19b137a28cb0af9a97
Lhaniii <ilhan95210@icloud.com>
2024-08-16 10:35:32 +0200

    task

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Annotate the below function’s parameters and 
+"""Annotate the below function’s parameters and
 return values with the appropriate types"""
 from typing import List, Tuple, Iterable, Sequence
 
